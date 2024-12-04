eca8962d59d8e152402d554f1225b33286981ec5
jokerf6 <fhakem75@gmail.com>
2024-12-05 00:39:55 +0200

    add database

--- .next/static/chunks/app/products/page-0fdf04d562e592f9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[571],{3383:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,83449,23)),Promise.resolve().then(n.bind(n,97331)),Promise.resolve().then(n.bind(n,58874))}},e=>{var s=s=>e(e.s=s);e.O(0,[844,906,818,548,864,441,517,358],()=>s(3383)),_N_E=e.O()}]);
